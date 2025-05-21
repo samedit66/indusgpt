a7525f775c1da2143108931f2fdfd566804262b1
samedit66 <samedit66@yandex.ru>
2025-05-21 16:22:07 +0300

    feat(chat): update imports

--- src/chat/__init__.py
+++ src/chat/__init__.py
@@ -1,15 +1,22 @@
 from .chat_manager import ChatManager
-from .question_list import QuestionList
+from .chat_state import State, StateType
+from .question_list import QuestionList, Question, QaPair
 from .user_answer_storage import UserAnswerStorage
-from .generate_response import generate_response
+from .generate_response import ResponseToUser, generate_response
 from .generate_reply import generate_reply
-from .info_extractor import extract_info
+from .info_extractor import UserInformation, extract_info
 
 
 __all__ = [
     "ChatManager",
+    "State",
+    "StateType",
     "QuestionList",
+    "Question",
+    "QaPair",
     "UserAnswerStorage",
+    "ResponseToUser",
+    "UserInformation",
     "generate_response",
     "generate_reply",
     "extract_info",
