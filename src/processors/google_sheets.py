7b4a72172e24863a36e83fb25eb352097b58b1c3
samedit66 <samedit66@yandex.ru>
2025-05-29 16:19:46 +0300

    feat(processors): google sheets processor to store Q&As

--- src/processors/google_sheets.py
+++ src/processors/google_sheets.py
@@ -1,64 +1,15 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-from src.chat import QaPair
 from src.chat.info_extractor import UserInformation, extract_info
 from src.persistence.models import User
+from src import types
 
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive",
 ]
-
-
-class GoogleSheetsProcessor:
-    """Processor that writes Q&A data to Google Sheets."""
-
-    def __init__(
-        self, credentials_path: str, sheet_url: str, worksheet_name: str
-    ) -> None:
-        """Initialize the Google Sheets processor.
-
-        If such worksheet does not exist, the processor will create a new worksheet with the given name.
-
-        Args:
-            credentials_path: Path to Google credentials file
-            sheet_url: URL of the Google Sheet to write to
-            worksheet_name: Name of the worksheet within the sheet
-        """
-        self.credentials_path = credentials_path
-        self.sheet_url = sheet_url
-        self.worksheet_name = worksheet_name
-        self.client = get_client(credentials_path)
-
-    async def __call__(self, user_id: int, qa_pairs: list[QaPair]) -> None:
-        """Process Q&A pairs by writing them to Google Sheets.
-
-        Args:
-            user_id: The ID of the user whose Q&A pairs are being processed
-            qa_pairs: List of question-answer pairs to process
-        """
-        user_name = (await User.filter(id=user_id).first()).name
-        user_info = await extract_info(qa_pairs)
-
-        # Open the sheet by URL
-        sheet = self.client.open_by_url(self.sheet_url)
-
-        # Open or create the worksheet
-        try:
-            worksheet = sheet.worksheet(self.worksheet_name)
-        except gspread.WorksheetNotFound:
-            worksheet = sheet.add_worksheet(
-                title=self.worksheet_name, rows="100", cols="20"
-            )
-            # Set up header row
-            headers = list(flatten_user_info(user_name, user_info).keys())
-            worksheet.append_row(headers)
-
-        # Prepare and append row data
-        row = list(flatten_user_info(user_name, user_info).values())
-        worksheet.append_row(row)
 
 
 def get_client(credentials_path: str) -> gspread.Client:
@@ -110,3 +61,52 @@ def flatten_user_info(user_name: str, user_info: UserInformation) -> dict:
         # Profit sharing
         "profit_sharing": user_info.profit_sharing.agreement,
     }
+
+
+class GoogleSheetsProcessor:
+    """Processor that writes Q&A data to Google Sheets."""
+
+    def __init__(
+        self, credentials_path: str, sheet_url: str, worksheet_name: str
+    ) -> None:
+        """Initialize the Google Sheets processor.
+
+        If such worksheet does not exist, the processor will create a new worksheet with the given name.
+
+        Args:
+            credentials_path: Path to Google credentials file
+            sheet_url: URL of the Google Sheet to write to
+            worksheet_name: Name of the worksheet within the sheet
+        """
+        self.credentials_path = credentials_path
+        self.sheet_url = sheet_url
+        self.worksheet_name = worksheet_name
+        self.client = get_client(credentials_path)
+
+    async def __call__(self, user_id: int, qa_pairs: list[types.QaPair]) -> None:
+        """Process Q&A pairs by writing them to Google Sheets.
+
+        Args:
+            user_id: The ID of the user whose Q&A pairs are being processed
+            qa_pairs: List of question-answer pairs to process
+        """
+        user_name = (await User.filter(id=user_id).first()).name
+        user_info = await extract_info(qa_pairs)
+
+        # Open the sheet by URL
+        sheet = self.client.open_by_url(self.sheet_url)
+
+        # Open or create the worksheet
+        try:
+            worksheet = sheet.worksheet(self.worksheet_name)
+        except gspread.WorksheetNotFound:
+            worksheet = sheet.add_worksheet(
+                title=self.worksheet_name, rows="100", cols="20"
+            )
+            # Set up header row
+            headers = list(flatten_user_info(user_name, user_info).keys())
+            worksheet.append_row(headers)
+
+        # Prepare and append row data
+        row = list(flatten_user_info(user_name, user_info).values())
+        worksheet.append_row(row)
