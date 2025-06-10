cae35afbe84cd16258aac2996e2497b91ab1af0a
samedit66 <samedit66@yandex.ru>
2025-06-10 17:29:20 +0300

    feat(processor): processor to create xlsx tables

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,5 +1,11 @@
 from .google_sheets import GoogleSheetsProcessor
 from .pdf import PdfProcessor
 from .airtable import AirtableProcessor
+from .excel import ExcelProcessor
 
-__all__ = ["GoogleSheetsProcessor", "PdfProcessor", "AirtableProcessor"]
+__all__ = [
+    "GoogleSheetsProcessor",
+    "PdfProcessor",
+    "AirtableProcessor",
+    "ExcelProcessor",
+]
