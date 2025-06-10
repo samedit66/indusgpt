d0f0c54f6378b93c546a19e00639ed8410ce3ec3
samedit66 <samedit66@yandex.ru>
2025-06-10 16:42:07 +0300

    chore(processors): rename

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,5 +1,5 @@
 from .google_sheets import GoogleSheetsProcessor
-from .pdf_processor import PdfProcessor
+from .pdf import PdfProcessor
 from .airtable import AirtableProcessor
 
 __all__ = ["GoogleSheetsProcessor", "PdfProcessor", "AirtableProcessor"]
